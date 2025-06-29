3320b4b9efe565b7a43c26b76bf5fb47ecc4075d
Kapil Gaire <Kap@KapilMacBook-Pro.local>
2025-06-29 17:18:39 +0545

    --append

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,7 +4,7 @@ function Header({ coinSearch, openWatchlist, titleClick }) {
   const [searchChar, SetSearchChar] = useState('')
   return (
     <div>
-      <div style={{ display: 'flex', justifyContent: 'end', columnGap: '27%', paddingRight: '2%', alignItems: 'center' }}>
+      <div style={{ display: 'flex', justifyContent: 'end', columnGap: '40%', paddingRight: '2%', alignItems: 'center' }}>
         <h2 style={{ backgroundColor: '#e0ffff', cursor: 'pointer' }}><a onClick={titleClick}>Coin Market</a></h2>
         <p><button onClick={() => (openWatchlist(true))} style={{ border: 'none', borderRadius: '10px', fontSize: '15px', cursor: 'pointer' }}>Watchlist</button></p>
 
